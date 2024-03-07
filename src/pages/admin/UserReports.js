99fd471d34c0dc6d2a55d89eb8a5232ce93bf776
iBel251 <ibbimran7@gmail.com>
2024-03-07 22:32:01 +0300

    fixed admin errors

--- src/pages/admin/UserReports.js
+++ src/pages/admin/UserReports.js
@@ -15,35 +15,36 @@ const userReports = ({ userReports, formatDate }) => {
       <Typography variant="h4" gutterBottom>
         User Reports
       </Typography>
-      {Object.entries(userReports).map(([houseId, reports], index) => {
-        if (houseId === "id") return null; // Skip the 'id' key
-        return (
-          <Paper
-            elevation={3}
-            key={houseId}
-            sx={{ marginBottom: 2, padding: 2 }}
-          >
-            <Typography variant="h6">User ID: {houseId}</Typography>
-            <List>
-              {reports.map((report, reportIndex) => (
-                <React.Fragment key={reportIndex}>
-                  <ListItem alignItems="flex-start">
-                    <ListItemText
-                      primary={`Reported by: ${report.reporterId}`}
-                      secondary={`Date: ${formatDate(
-                        report.date.seconds
-                      )} - Message: ${report.message}`}
-                    />
-                  </ListItem>
-                  {reportIndex < reports.length - 1 && (
-                    <Divider variant="inset" component="li" />
-                  )}
-                </React.Fragment>
-              ))}
-            </List>
-          </Paper>
-        );
-      })}
+      {userReports &&
+        Object.entries(userReports).map(([houseId, reports], index) => {
+          if (houseId === "id") return null; // Skip the 'id' key
+          return (
+            <Paper
+              elevation={3}
+              key={houseId}
+              sx={{ marginBottom: 2, padding: 2 }}
+            >
+              <Typography variant="h6">User ID: {houseId}</Typography>
+              <List>
+                {reports.map((report, reportIndex) => (
+                  <React.Fragment key={reportIndex}>
+                    <ListItem alignItems="flex-start">
+                      <ListItemText
+                        primary={`Reported by: ${report.reporterId}`}
+                        secondary={`Date: ${formatDate(
+                          report.date.seconds
+                        )} - Message: ${report.message}`}
+                      />
+                    </ListItem>
+                    {reportIndex < reports.length - 1 && (
+                      <Divider variant="inset" component="li" />
+                    )}
+                  </React.Fragment>
+                ))}
+              </List>
+            </Paper>
+          );
+        })}
     </Box>
   );
 };
